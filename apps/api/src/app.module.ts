2cd62334ba0b5394cfd864970c1f0aed5aceceba
Matthew Tedunjaiye <tedunjaiyem@gmail.com>
2026-01-09 16:19:26 +0100

    Added Axios package and fully enabled disabled project and donation modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaModule } from './common/prisma.module';
-// Other modules remain commented until built
-// import { WalletModule } from './modules/wallet/wallet.module';
-// import { DonationModule } from './modules/donation/donation.module';
-// import { ProjectModule } from './modules/project/project.module';
+import { WalletModule } from './modules/wallet/wallet.module';
+import { DonationModule } from './modules/donation/donation.module';
+import { ProjectModule } from './modules/project/project.module';
 
 @Module({
   imports: [
@@ -13,10 +12,10 @@ import { PrismaModule } from './common/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
-    AuthModule, // <--- ENABLED
-    // WalletModule,
-    // DonationModule,
-    // ProjectModule,
+    AuthModule,
+    WalletModule,
+    DonationModule,
+    ProjectModule,
   ],
   controllers: [],
   providers: [],
