fa33db54c7809d532c9993183c4db71a5447d817
Matthew Tedunjaiye <tedunjaiyem@gmail.com>
2026-01-11 22:53:48 +0100

    feat(api): implement atomic fulfillment of direct donations via webhook

--- apps/api/src/modules/wallet/wallet.module.ts
+++ apps/api/src/modules/wallet/wallet.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { WalletController } from './wallet.controller';
 import { WalletService } from './wallet.service';
 import { WalletRepository } from './wallet.repository';
+import { DonationModule } from '../donation/donation.module';
+import { DonationService } from '../donation/donation.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, DonationModule],
   controllers: [WalletController],
   providers: [WalletService, WalletRepository],
-  exports: [WalletService, WalletRepository],
+  exports: [DonationService, WalletService, WalletRepository],
 })
 export class WalletModule {}
