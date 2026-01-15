c0d161077c2751d7a53265f0c1f51d853c49e451
Matthew Tedunjaiye <tedunjaiyem@gmail.com>
2026-01-15 17:59:44 +0100

    feat(security): add password strength validation and db fields for account lockout and email verification

--- apps/api/src/modules/auth/dto/auth.dto.ts
+++ apps/api/src/modules/auth/dto/auth.dto.ts
@@ -1,5 +1,6 @@
 import { IsEmail, IsEnum, IsString, MaxLength, MinLength } from 'class-validator';
 import { Currency } from '@givar/database';
+import { IsStrongPassword } from 'src/common/decorators/is-strong-password-decorator';
 
 export class RegisterDto {
   @IsEmail({}, { message: 'Please provide a valid email address' })
@@ -8,6 +9,7 @@ export class RegisterDto {
   @IsString()
   @MinLength(8, { message: 'Password must be at least 8 characters long' })
   @MaxLength(32, { message: 'Password is too long' })
+  @IsStrongPassword()
   password!: string;
 
   @IsString()
