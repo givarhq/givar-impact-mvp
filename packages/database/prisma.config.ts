dcccc6874fae4da6b197e3c90794a378eeab024d
Matthew Tedunjaiye <tedunjaiyem@gmail.com>
2026-01-17 05:31:27 +0100

    Add seed command

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -4,7 +4,9 @@ import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-
+  migrations: {
+    seed: 'tsx prisma/seed.ts'
+  },
   datasource: {
     url: process.env.DATABASE_URL!,
   },
